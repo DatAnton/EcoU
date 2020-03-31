367c36108c4cc458d9c6dc129c745cf2243d5219
Anton Dacenkof <datanton@MacBook-Pro-Anton.local>
2020-04-01 00:23:21 +0300

    Initial commit with initialize DB

---
+++ Models/ProjectContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoU.Models
+{
+    public class ProjectContext : IdentityDbContext<User>
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+namespace EcoU.Models
+{
+    public class User : IdentityUser
+    {
+        public string LifeMessage { get; set; }
+
+    }
+}
